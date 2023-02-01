28a5b90882c3738b04e3ad85744a2574f96703c7
Zisis Kostakakis <zisis.kostakakis@stormharvester.com>
2023-02-01 21:04:02 +0000

    common_funcs updates

--- utils/common_funcs.py
+++ utils/common_funcs.py
@@ -5,10 +5,11 @@ import boto3
 def get_aws_creds():
     """Get AWS credentials from ~/.aws/credentials file"""
     with open(f'{AWS_CREDS_DIR}{AWS_CREDS_FILE}', 'r') as f:
-        lines = f.readlines()
-    aws_access_key_id = lines[1].split('=')[1].strip()
-    aws_secret_access_key = lines[2].split('=')[1].strip()
-    return aws_access_key_id, aws_secret_access_key
+        # get the credentials in the [webapp] section
+        aws_creds = f.readlines()[2:4]
+        aws_access_key_id = aws_creds[0].split('=')[1].strip()
+        aws_secret_access_key = aws_creds[1].split('=')[1].strip()
+        return aws_access_key_id, aws_secret_access_key
 
 
 def get_boto3_session():
@@ -29,17 +30,17 @@ def get_s3_client():
     return s3
 
 
-def read_object_from_s3(bucket_name, object_name):
+def read_object_from_s3(bucket_name, object_name, s3_client=None):
     """Read object from S3 bucket"""
-    s3 = get_s3_client()
+    s3 = get_s3_client() if s3_client is None else s3_client
     obj = s3.get_object(Bucket=bucket_name, Key=object_name)
     return obj
 
 
-def write_object_to_s3(bucket_name, object_name, data):
+def write_object_to_s3(bucket_name, object_name, data, s3_client=None):
     """Write object to S3 bucket"""
-    s3 = get_s3_client()
-    s3.put_object(Bucket=bucket_name, Key=object_name, Body=data)
+    s3_client = get_s3_client() if s3_client is None else s3_client
+    s3_client.put_object(Bucket=bucket_name, Key=object_name, Body=data)
 
 
 def get_ddb_client():
@@ -49,41 +50,37 @@ def get_ddb_client():
     return ddb
 
 
-def get_ddb_client():
-    """Get boto3 DynamoDB client"""
-    session = get_boto3_session()
-    ddb = session.client('dynamodb')
-    return ddb
-
-
-def get_ddb_object(ddb, table_name, key):
+def get_ddb_object(ddb_client, table_name, key):
     """Get DynamoDB object"""
-    response = ddb.get_item(
+    ddb_client = get_ddb_client() if ddb_client is None else ddb_client
+    response = ddb_client.get_item(
         TableName=table_name,
         Key=key
     )
     return response
 
 
-def write_ddb_object(ddb, table_name, item):
+def write_ddb_object(ddb_client, table_name, item):
     """Write DynamoDB object"""
-    response = ddb.put_item(
+    ddb_client = get_ddb_client() if ddb_client is None else ddb_client
+    response = ddb_client.put_item(
         TableName=table_name,
         Item=item
     )
     return response
 
 
-def write_object_to_both_s3_and_ddb(bucket_name, object_name, data, table_name, key):
+def write_object_to_both_s3_and_ddb(bucket_name, object_name, data, table_name, key, s3_client=None, ddb_client=None):
     """Write object to S3 and DynamoDB"""
-    write_object_to_s3(bucket_name, object_name, data)
-    ddb = get_ddb_client()
-    write_ddb_object(ddb, table_name, key)
+    s3_client = get_s3_client() if s3_client is None else s3_client
+    ddb_client = get_ddb_client() if ddb_client is None else ddb_client
+    write_object_to_s3(bucket_name, object_name, data, s3_client)
+    write_ddb_object(ddb_client, table_name, key)
 
 
-def check_if_object_exists_in_s3(bucket_name, object_name):
+def check_if_object_exists_in_s3(bucket_name, object_name, s3_client=None):
     """Check if object exists in S3"""
-    s3 = get_s3_client()
+    s3 = get_s3_client() if s3_client is None else s3_client
     try:
         s3.head_object(Bucket=bucket_name, Key=object_name)
         return True
@@ -91,19 +88,20 @@ def check_if_object_exists_in_s3(bucket_name, object_name):
         return False
 
 
-def check_if_object_exists_in_ddb(ddb, table_name, key):
+def check_if_object_exists_in_ddb(ddb_client, table_name, key):
     """Check if object exists in DynamoDB"""
+    ddb_client = get_ddb_client() if ddb_client is None else ddb_client
     try:
-        get_ddb_object(ddb, table_name, key)
+        get_ddb_object(ddb_client, table_name, key)
         return True
     except:
         return False
 
 
-def check_if_object_exists_in_both_s3_and_ddb(bucket_name, object_name, table_name, key):
+def check_if_object_exists_in_both_s3_and_ddb(bucket_name, object_name, table_name, key, s3_client=None, ddb_client=None):
     """Check if object exists in S3 and DynamoDB"""
-    s3 = get_s3_client()
-    ddb = get_ddb_client()
+    s3 = get_s3_client() if s3_client is None else s3_client
+    ddb = get_ddb_client() if ddb_client is None else ddb_client
     try:
         s3.head_object(Bucket=bucket_name, Key=object_name)
         get_ddb_object(ddb, table_name, key)
@@ -112,40 +110,41 @@ def check_if_object_exists_in_both_s3_and_ddb(bucket_name, object_name, table_na
         return False
 
 
-def get_list_of_buckets():
+def get_list_of_buckets(s3_client=None):
     """Get list of buckets"""
-    s3 = get_s3_client()
+    s3 = get_s3_client() if s3_client is None else s3_client
     response = s3.list_buckets()
     return response
 
 
-def get_list_of_objects_s3(bucket_name):
+def get_list_of_objects_s3(bucket_name, s3_client=None):
     """Get list of objects in bucket"""
-    s3 = get_s3_client()
+    s3 = get_s3_client() if s3_client is None else s3_client
     response = s3.list_objects_v2(Bucket=bucket_name)
     return response
 
 
-def get_list_of_objects_ddb(ddb, table_name):
+def get_list_of_objects_ddb(ddb_client, table_name):
     """Get list of objects in DynamoDB table"""
-    response = ddb.scan(TableName=table_name)
+    ddb_client = get_ddb_client() if ddb_client is None else ddb_client
+    response = ddb_client.scan(TableName=table_name)
     return response
 
 
-def get_object_from_s3(bucket_name, object_name):
+def get_object_from_s3(bucket_name, object_name, s3_client=None):
     """Get object from S3"""
-    s3 = get_s3_client()
+    s3 = get_s3_client() if s3_client is None else s3_client
     obj = s3.get_object(Bucket=bucket_name, Key=object_name)
     return obj
 
 
-def write_object_to_s3(bucket_name, object_name, data):
+def write_object_to_s3(bucket_name, object_name, data, s3_client=None):
     """Write object to S3"""
-    s3 = get_s3_client()
+    s3 = get_s3_client() if s3_client is None else s3_client
     s3.put_object(Bucket=bucket_name, Key=object_name, Body=data)
 
 
-def delete_object_from_s3(bucket_name, object_name):
+def delete_object_from_s3(bucket_name, object_name, s3_client=None):
     """Delete object from S3"""
-    s3 = get_s3_client()
+    s3 = get_s3_client() if s3_client is None else s3_client
     s3.delete_object(Bucket=bucket_name, Key=object_name)
